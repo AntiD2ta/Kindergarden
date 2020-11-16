d419ad25e8e762c79a4abcfc7bbe81856c4bbdff
AntiD2ta <mtpotro41@gmail.com>
2020-11-16 11:42:03 -0500

    [agent] - Implement `Robot` abstract class

--- src/agent/robot.py
+++ src/agent/robot.py
@@ -1,2 +1,12 @@
 class Robot:
-    pass
+    def __init__(self, pos):
+        self.pos = pos
+        self.garbage_collected = 0
+        self.will_clean = False
+        self.carrying_baby = False
+
+    def action(house):
+        pass
+
+    def move(self, house, pos, steps=1):
+        pass
