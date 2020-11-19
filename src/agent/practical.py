724742e73df368eeed5e66bb7e00061c68d60b80
AntiD2ta <mtpotro41@gmail.com>
2020-11-19 01:49:58 -0500

    [agent] - Upgrade Practical agent to learn `t` value of env

--- src/agent/practical.py
+++ src/agent/practical.py
@@ -18,31 +18,31 @@ class Practical(Reagent):
     def __init__(self, pos, t):
         global log
         log = Logger('Kindergarden').getChild('Practical')
+        self.t = 0
+        self.time = 0
         self.babies = 0
         self.mode = ''
         self.drop = False
+        self.old_pos = pos
         super().__init__(pos)
 
     def action(self, house):
-        t1 = time()
         beliefs   = self.get_beliefs(house)
-        t2 = time()
-        log.debug(f'Delay of beliefs: {round(t2 - t1, 3)}')
         options   = self.get_options(beliefs)
-        t1 = time()
-        log.debug(f'Delay of options: {round(t1 - t2, 3)}')
         operation = self.filter_options(options)
-        t2 = time()
-        log.debug(f'Delay of filter_options: {round(t2 - t1, 3)}')
         self.execute(operation, house)
-        t1 = time()
-        log.debug(f'Delay of execute: {round(t1 - t2, 3)}')
 
     def get_beliefs(self, house):
         '''
         Get state of the environment detected by the agent given the latest perception
         '''
         beliefs = dict()
+        self.time += 1
+
+        #Learn t
+        if self.old_pos != self.pos:
+            self.t = self.time - self.t
+            self.time = 0
         #Top priority intentions
         if self.will_clean or self.carrying_baby:
             return beliefs
@@ -51,24 +51,19 @@ class Practical(Reagent):
         self.babies = count_free_babies(house)
         n, m = get_length(house)
         beliefs['mess'] = count_dirt(house) * 100 // (n * m)
-        beliefs['dirt_groups'] = group_target(house, DIRT, self.t // 2)
+        beliefs['dirt_groups'] = group_target(house, DIRT, self.t // 2 if self.t > 0 else 10)
         beliefs['baby_groups'] = group_target(house, BABY, 2)
 
         paths_to_babies = list()
         paths_to_dirt = list()
 
-        t1 = time()
         max_path = len(closest_target(house, self.pos, DIRT, [OBSTACLE]))
         max_path *= 2
         paths_to_target(house, self.pos, self.pos, DIRT, {self.pos}, dict(), [OBSTACLE, BABY], paths_to_dirt, max_path)
-        t2 = time()
-        log.debug(f'Delay of path to dirt: {round(t2 - t1, 3)}')
 
         max_path = len(closest_target(house, self.pos, BABY, [OBSTACLE]))
-        max_path += max_path // 2
+        max_path += self.t // 2 if self.t > 0 else 5
         paths_to_target(house, self.pos, self.pos, BABY, {self.pos}, dict(), [OBSTACLE], paths_to_babies, max_path)
-        t1 = time()
-        log.debug(f'Delay of path to baby: {round(t1 - t2, 3)}')
         # log.debug(f'paths_to_babies: {paths_to_babies}')
         # log.debug(f'paths_to_dirt: {paths_to_dirt}')
 
@@ -181,6 +176,7 @@ class Practical(Reagent):
         '''
         x, y = self.pos
         task, args = operation
+        
         if task is 'CLEAN':
             house[x][y].update(ROBOT)
             house[x][y].dirty = False
@@ -216,6 +212,8 @@ class Practical(Reagent):
                 self.carrying_baby = True
                 house[x][y].update(f'{ROBOT}-{BABY}')
                 log.debug(f'Loaded baby on ({x}, {y})', 'execute')
+        
+        self.old_pos = self.pos
 
     def execute_move(self, operation, house):
         task, args = operation
