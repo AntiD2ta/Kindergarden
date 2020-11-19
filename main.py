188480d9588503961ee81ec8817cf338093fd8b9
AntiD2ta <mtpotro41@gmail.com>
2020-11-19 01:50:11 -0500

    [main] - Update simulation suite

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ if __name__ == '__main__':
     import os
 
     parser = argparse.ArgumentParser(description='Kindergarden simulator')
-    parser.add_argument('-r', '--rows', type=int, default=7, help='number of rows of the house')
+    parser.add_argument('-r', '--rows', type=int, default=9, help='number of rows of the house')
     parser.add_argument('-c', '--columns', type=int, default=8, help='number of columns of the house')
     parser.add_argument('-d', '--dirtiness', type=int, default=30, help='percentage of dirty cells at the start of the simulation')
     parser.add_argument('-o', '--obstacules', type=int, default=20, help='percentage of obstacules at the start of the simulation')
@@ -90,7 +90,7 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--time', type=int, default=5, help='number of turns between changes of the environment')
     parser.add_argument('-p', '--practical', type=bool, const=True, nargs='?', help='set if you want to simulate Practical agent. Reagent agent is default')
     parser.add_argument('-P', '--bernoulli', type=float, default=0.5, help='probability of a baby moving in an environment change (0 to 1)')
-    parser.add_argument('-l', '--level', type=str, default='ERROR', help='log level')
+    parser.add_argument('-l', '--level', type=str, default='INFO', help='log level')
     parser.add_argument('-f', '--file', type=bool, const=True, nargs='?', help='set if you want log to a file')
     parser.add_argument('-i', '--interactive', type=bool, const=True, nargs='?', help='set if you want to see what happens in every turn')
     parser.add_argument('-s', '--simulation', type=bool, const=True, nargs='?', help='set if you want to simulate all the agents')
--- simulate.sh
+++ simulate.sh
@@ -3,46 +3,53 @@ if [ ! -d ./results ]; then
     mkdir results
     if [ ! -f ./results/results.txt ]; then
         touch ./results/results.txt
+    else
+        rm ./results/results.txt
+        touch ./results/results.txt
     fi
 fi
 
 declare -a simulation
 #default (easy)
-simulation[1]='-s'
-#easy, more dirtiness
-simulation[2]='-d 45'
-#easy, more obstacules
-simulation[3]='-o 30'
-#easy, more babies and less dirtiness
-simulation[4]='-b 8 -d 15'
-#normal, time 2
-simulation[5]='-t 2 -s'
-#normal, time 2 and moving babies with 0.75
-simulation[6]='-t 2 -P 0.75'
-#normal, time 2, moving babies with 0.75 and dirtiness at 45%
-simulation[7]='-t 2 -P 0.75 -d 45'
-#hard, time 1
-simulation[8]='-t 1 -s'
-#hard, time 1 and moving babies with 0.75
-simulation[9]='-t 1 -P 0.75'
-#hard, time 1, moving babies with 0.75 and dirtiness at 45%
-simulation[10]='-t 1 -P 0.75 -d 45'
-#hard, time 1
-simulation[11]='-t 1 -s'
-#hard, time 1 and moving babies with 1
-simulation[12]='-t 1 -P 1'
-#hard, time 1, moving babies with 1, dirtiness at 10%, obtacules at 10%
-simulation[13]='-t 1 -P 1 -d 10 -o 10'
-#ultimate hard, time 1, moving babies with 0.5, dirtiness at 40%, obtacules at 20% and 6 babies
-simulation[14]='-t 1 -d 40 -b 6'
-#ultimate hard, time 1, moving babies with 1, dirtiness at 40%, obtacules at 20% and 6 babies
+simulation[1]=''
+#easy, t = 6, more dirtiness
+simulation[2]='-t 6 -d 45'
+#easy, t = 6, more obstacules
+simulation[3]='-t 6 -o 30'
+#easy, t = 6,more babies and less dirtiness
+simulation[4]='-t 6 -b 8 -d 15'
+#normal, t = 4
+simulation[5]='-t 4'
+#normal, t = 4 and moving babies with 0.75
+simulation[6]='-t 4 -P 0.75'
+#normal, t = 4, moving babies with 0.75 and dirtiness at 45%
+simulation[7]='-t 4 -P 0.75 -d 45'
+#hard, t = 3
+simulation[8]='-t 3'
+#hard, t = 2
+simulation[9]='-t 2'
+#hard, t = 2 and moving babies with 0.75
+simulation[10]='-t 2 -P 0.75'
+#hard, t = 2, moving babies with 0.75 and dirtiness at 45%
+simulation[11]='-t 2 -P 0.75 -d 45'
+#hard, t = 2 and moving babies with 1
+simulation[12]='-t 2 -P 1'
+#hard, t = 2, moving babies with 1, dirtiness at 10%, obtacules at 10%
+simulation[13]='-t 2 -P 1 -d 10 -o 10'
+#hard, t = 2, moving babies with 0.5, dirtiness at 40%, obtacules at 20% and 6 babies
+simulation[14]='-t 2 -d 40 -b 6'
+#very hard, t = 2, moving babies with 0.5, dirtiness at 40%, obtacules at 20% and 8 babies
+simulation[15]='-t 2 -d 10 -b 8'
+#insane, t = 1, moving babies with 1, dirtiness at 40%, obtacules at 20% and 6 babies
 simulation[15]='-t 1 -P 1 -d 40 -b 6'
 
-for i in {1..12}; do
+for i in {1..15}; do
+    printf "Set %d \n\n" $i
     printf "Set %d: \n\n" $i >>./results/results.txt
     for j in {1..30}; do
+        printf "Simulation %d \n" $j
         printf "Simulation %d: \n\n" $j >>./results/results.txt
-        echo | python main.py ${simulation[i]} -s >>./results/results.txt
+        echo | python main.py ${simulation[i]} -s -f >>./results/results.txt
     done
 done
 
